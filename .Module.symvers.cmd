8bf10a99dc410bafb0183c791abcc3c9fb939ab1
alexboc10 <a.boccini10@gmail.com>
2021-06-12 19:40:52 +0200

    update

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/altair9510/university/SOA_project/Module.symvers := sed 's/ko$$/o/' /home/altair9510/university/SOA_project/modules.order | scripts/mod/modpost -m -a   -o /home/altair9510/university/SOA_project/Module.symvers -e -i Module.symvers   -T -
--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/altair9510/university/SOA_project/modules.order := {   echo /home/altair9510/university/SOA_project/TBDE.ko; :; } | awk '!x[$$0]++' - > /home/altair9510/university/SOA_project/modules.order
